3c8d9478c6da32bfcda9196ad959240e5e9b55c9
KOUAMLAN <evlekouamlanwilliams@yahoo.fr>
2025-09-18 19:40:59 +0200

    Mise à jour package.json et vite.config.js pour déploiement

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Site_internet/', // Nom exact du dépôt avec slash final
+  base: '/',  // Racine car domaine personnalisé
   plugins: [react()]
-})
+});
