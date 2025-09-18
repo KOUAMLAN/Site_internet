2ae28aa9f0341aaecc8b41cb3faa9859a5a7782c
KOUAMLAN <evlekouamlanwilliams@yahoo.fr>
2025-09-18 08:25:05 +0200

    Build et préparation déploiement GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/enseignement-pour-tous/', // Chemin relatif pour GitHub Pages
+  base: '/Site_internet/', // Nom exact du dépôt avec slash final
   plugins: [react()]
 })
